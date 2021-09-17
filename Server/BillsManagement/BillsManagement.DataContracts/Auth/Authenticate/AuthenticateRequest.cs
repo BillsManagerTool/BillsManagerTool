e4f9464e0325fa21e8f5dd374a73651b11621207
Georgi Cholakov <cholakovge@gmail.com>
2021-09-18 01:29:47 +0200

    Removed datacontracts proj

--- Server/BillsManagement/BillsManagement.DataContracts/Auth/Authenticate/AuthenticateRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿//namespace BillsManagement.DataContracts.Auth
-//{
-//    public class AuthenticateRequest
-//    {
-//        public string Email { get; set; }
-
-//        public string Password { get; set; }
-//    }
-//}
--- Server/BillsManagement/BillsManagement.DataContracts/Auth/Register/RegisterRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿//namespace BillsManagement.DataContracts.Auth
-//{
-//    public class RegisterRequest
-//    {
-//        public string Email { get; set; }
-
-//        public string Password { get; set; }
-//    }
-//}
