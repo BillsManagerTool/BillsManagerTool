4db35709ac529fe070ae73d958f892dd2a9ccefc
gecho <cholakovge@gmail.com>
2021-09-18 12:47:51 +0200

    MS Tests project with 1 successfully ran test

---
+++ Server/BillsManagement/BillsManagement.Custom/Swagger/SwaggerMiddleware.cs
@@ -0,0 +1,23 @@
+﻿//using Microsoft.Extensions.DependencyInjection;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace BillsManagement.Custom.Swagger
+//{
+//    public static class SwaggerMiddleware
+//    {
+//        public static IServiceCollection AddFluentValidation(this IServiceCollection services)
+//        {             //...add 
+//            services.AddSwaggerGen(c =>
+//            {
+//                c.SwaggerDoc(_assemblyVersion, new OpenApiInfo { Title = _assemblyName, Version = _assemblyVersion });
+//                c.IncludeXmlComments($"{_applicationBaseDirectory}\\{ApiConstants.CoreXmlDocumentation}");
+//                c.IncludeXmlComments($"{_applicationBaseDirectory}\\{ApiConstants.DomainModelsXmlDocumentation}");
+//            });
+//            return services;
+//        }
+//    }
+//}
---
+++ Server/BillsManagement/BillsManagement.Tests/OccupantsControllerTests.cs
@@ -0,0 +1,57 @@
+﻿using BillsManagement.API.Controllers;
+using BillsManagement.DomainModel;
+using BillsManagement.Services.ServiceContracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace BillsManagement.Tests
+{
+    [TestClass]
+    public class OccupantsControllerTests
+    {
+        private Mock<IOccupantService> _service;
+        private OccupantsController _controller;
+
+        [TestInitialize]
+        public void Init()
+        {
+            this._service = new Mock<IOccupantService>();
+            this._controller = new OccupantsController(this._service.Object);
+
+            this._controller.ControllerContext = new ControllerContext();
+            this._controller.ControllerContext.HttpContext = new DefaultHttpContext();
+
+            // Arrange
+            this._service.Setup(x => x.GetOccupantDetailsById(It.IsAny<int>()))
+                .Returns(new DetailedOccupant());
+        }
+
+        [TestMethod]
+        public void GetOccupant_OccupantFound_ShouldReturnDetailedOccupant()
+        {
+            // Arrange            
+            this._controller.HttpContext.Items["UserId"] = 123;
+            var occupantId = int.Parse(this._controller.HttpContext.Items["UserId"].ToString());
+
+            var expectedResult = new DetailedOccupant()
+            {
+                OccupantId = 123
+            };
+
+            this._service.Setup(x => x.GetOccupantDetailsById(occupantId))
+                .Returns(expectedResult);
+
+            // Act
+            var result = this._controller.GetOccupant();
+            var actualResult = (DetailedOccupant)((ActionResult<DetailedOccupant>)result).Value;
+
+            // Assert
+            Assert.AreEqual(expectedResult.OccupantId, actualResult.OccupantId);
+        }
+    }
+}
