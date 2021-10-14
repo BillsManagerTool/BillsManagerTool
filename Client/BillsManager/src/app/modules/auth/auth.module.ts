8a6a7257a8aea3da5a21a13f41cb05d24d9f4a2a
gecho <cholakovge@gmail.com>
2021-10-14 22:44:12 +0200

    auth interceptor

--- Client/BillsManager/src/app/modules/auth/auth.module.ts
+++ Client/BillsManager/src/app/modules/auth/auth.module.ts
@@ -2,9 +2,18 @@ import { AuthRoutingModule } from './auth-routing.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthComponent } from './auth/auth.component';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptor } from './auth.interceptor';
 
 @NgModule({
   declarations: [AuthComponent],
   imports: [CommonModule, AuthRoutingModule],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
+  ],
 })
 export class AuthModule {}
