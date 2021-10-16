5d7ea33f26379baa4beee7a2fa4b7247ac166b07
gecho <cholakovge@gmail.com>
2021-10-16 16:38:43 +0200

    external service for call to external apis

---
+++ Client/BillsManager/src/app/services/external.service.ts
@@ -0,0 +1,13 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ExternalService {
+  constructor(private http: HttpClient) {}
+
+  getCountries() {
+    return this.http.get<any>(`https://countriesnow.space/api/v0.1/countries`);
+  }
+}
