fe1e2a21cf76a6a5bd55a89c072f098ed7ec62e5
gecho <cholakovge@gmail.com>
2021-10-17 00:13:41 +0200

    header and footer initial

--- Client/BillsManager/src/app/app.component.ts
+++ Client/BillsManager/src/app/app.component.ts
@@ -1,8 +1,5 @@
-import { RegisterRequest } from './domain-models/registerRequest';
-import { IBaseResponse } from './interfaces/baseResponse';
-import { AuthService } from './services/auth.service';
+import { TranslateService } from './services/translate.service';
 import { Component, OnInit } from '@angular/core';
-import { AuthenticateResponse } from './domain-models/authenticateResponse';
 
 @Component({
   selector: 'app-root',
@@ -12,34 +9,9 @@ import { AuthenticateResponse } from './domain-models/authenticateResponse';
 export class AppComponent implements OnInit {
   title = 'BillsManager';
 
-  constructor(private authService: AuthService) {}
+  constructor(private translateService: TranslateService) {}
 
   ngOnInit() {
-    let registerRequest = new RegisterRequest();
-    registerRequest.Email = 'cholaka@gmail.com';
-    registerRequest.Password = 'cholaka1234';
-    registerRequest.BuildingAddress = 'Uoashbarn 61';
-    registerRequest.TownId = 1;
-    registerRequest.CountryId = 1;
-    registerRequest.EntranceNumber = 'A';
-    registerRequest.ApartmentNumber = '2C';
-    registerRequest.ApartmentFloor = 2;
-
-    this.authService
-      .register(registerRequest)
-      .subscribe((response: IBaseResponse) => {
-        console.log(response);
-      });
-
-    setTimeout(() => 3000);
-
-    let email = 'cholaka@gmail.com';
-    let password = 'cholaka1234';
-
-    this.authService
-      .authenticate(email, password)
-      .subscribe((response: AuthenticateResponse) => {
-        console.log(response);
-      });
+    this.translateService.translate('en-US');
   }
 }
--- Client/BillsManager/src/app/services/translate.service.ts
+++ Client/BillsManager/src/app/services/translate.service.ts
@@ -1,9 +1,18 @@
+import { JsonpClientBackend, HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import enUS from '../../localization/en-US.json';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TranslateService {
+  myData: any;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  translate(lenguage: string) {
+    if (lenguage === 'en-US') {
+      console.log(enUS);
+    }
+  }
 }
