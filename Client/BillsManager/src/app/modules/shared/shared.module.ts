43ab22e9fda1a9671c0ec1ac86a00a5ee5222088
gecho <cholakovge@gmail.com>
2021-10-16 13:20:45 +0200

    primeng styles configuration

--- Client/BillsManager/src/app/modules/shared/shared.module.ts
+++ Client/BillsManager/src/app/modules/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InputFieldComponent } from './custom-controls/input-field/input-field.component';
+import { CheckboxModule } from 'primeng/checkbox';
 
 @NgModule({
   declarations: [InputFieldComponent, InputFieldComponent],
-  imports: [CommonModule],
-  exports: [InputFieldComponent],
+  imports: [CommonModule, CheckboxModule],
+  exports: [InputFieldComponent, CheckboxModule],
 })
 export class SharedModule {}
